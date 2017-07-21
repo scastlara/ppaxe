df29f13205f425e1e5b376970f8faa548155eb68
Sergio Castillo Lara <s.cast.lara@gmail.com>
2017-07-21 13:29:35 +0200

    First setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ppaxe',
+      version='0.1',
+      description='PPI extractor from PubMed articles',
+      url='http://github.com/scastlara/ppaxe',
+      author='S. Castillo-Lara',
+      author_email='s.cast.lara@gmail.com',
+      license='GPL-3.0',
+      packages=['ppaxe'],
+      zip_safe=False)
+
